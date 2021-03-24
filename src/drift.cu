da2f17cd7d768b9fd11e482e656b40f87e5b1949
Dominic Marcello <dmarce504@gmail.com>
2021-03-24 15:05:38 -0500

    Restored accurate drift

--- src/drift.cu
+++ src/drift.cu
@@ -84,7 +84,7 @@ void drift_particles(particle_set parts, double dt, double a0, double a1, double
 	auto stream = get_stream();
 	parts.prepare_drift();
 //	const auto a = 1.0 / (0.5 / a0 + 0.5 / a1);
-//	const auto ddt = cosmos_drift_dtau(a0,dt);
+	const auto ddt = cosmos_drift_dtau(a0,dt);
 	const auto a = 1.0 / ddt;
 //	printf( "%e %e\n", a, 1.0/ddt);
 	double* results;
