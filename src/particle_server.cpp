6531265bffc79393dd5cce989e049d188295df6e
Dominic Marcello <dmarce504@gmail.com>
2021-05-22 09:02:51 -0500

    Fixed bug

--- src/particle_server.cpp
+++ src/particle_server.cpp
@@ -184,7 +184,7 @@ void particle_server::generate_random() {
 		}
 	}
 
-	parts->generate_random(123*hpx_rank() + 42);
+	parts->generate_random(time(NULL) + 1234*hpx_rank() + 42);
 
 	hpx::wait_all(futs.begin(), futs.end());
 }
